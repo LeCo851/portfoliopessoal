6306ef64083adc728096baee4dbc713fb7585adf
Leandro Coelho <leandro.coelho@edu.unirio.br>
2026-01-13 20:25:18 -0300

    feat: versao para prod do projeto2

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   // IMPORTANTE: Apenas '/api'.
   // O navegador vai completar automaticamente com o domínio atual.
-  apiUrl: 'https://portfoliopessoal-backend-395115142542.europe-west1.run.app/api'
+  apiUrl: 'https://portfoliopessoal-backend-395115142542.us-central1.run.app/api'
 };
