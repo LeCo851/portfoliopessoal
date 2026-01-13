d177c6fcac0dd6d533e406fe7d8ade2b2d669085
Leandro Coelho <leandro.coelho@edu.unirio.br>
2026-01-13 20:46:59 -0300

    feat: versao para prod do projeto4

---
+++ spring-app/src/main/java/com/leandrocoelho/portfoliopessoal/config/CorsConfig.java
@@ -0,0 +1,20 @@
+package com.leandrocoelho.portfoliopessoal.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:4200", // Para seus testes locais
+                        "https://portfoliopessoal-frontend-395115142542.us-central1.run.app" // <--- ADICIONE A URL DO SEU FRONTEND AQUI (sem a barra / no final)
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
